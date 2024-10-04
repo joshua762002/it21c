9a10d76b2279bd6dc896fa0fcb1efe85843628aa
ikenaat <kennethgamolo4@gmail.com>
2024-10-03 22:38:26 -1200

    added index.html and inline js with objects

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const person1 ={
+    name:"joshua eviota",
+    age:22,
+    occupation:"student nga hilomon"
+}
+
+//Process and Output
+console.log("Name: "+person1.name);
+console.log("Age: "+person1.age);
+console.log("Occupation: "+person1.occup);
